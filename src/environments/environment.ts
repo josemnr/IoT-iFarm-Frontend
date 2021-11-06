a7ebef762301049ce8cd35c31af79dd5ffd0e6df
josemnr <maninari97@gmail.com>
2021-11-05 21:06:08 -0600

    Update enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  authUrl: "http://localhost:3000/",
+  usersUrl: "http://localhost:3000/",
+  seedsUrl: "http://localhost:3000/",
+  calendarUrl: "http://localhost:3000/",
+  greenhousesUrl:"http://localhost:3000/"
 };
 
 /*
