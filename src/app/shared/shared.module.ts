53a4a354df2dce68e3f6d01cbb0e4c5f8cdf37f9
josemnr <maninari97@gmail.com>
2021-11-07 16:17:51 -0600

    Add ngbModule to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,21 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { MaterialModule } from './material/material.module';
+
+import { GreenhouseCardComponent } from './components/greenhouse-card/greenhouse-card.component';
 import { EditProfileDialogComponent } from './components/edit-profile-dialog/edit-profile-dialog.component';
 
 @NgModule({
   declarations: [
+    GreenhouseCardComponent,
     EditProfileDialogComponent
   ],
   imports: [
+    NgbModule,
     CommonModule,
     MaterialModule,
     ReactiveFormsModule
   ],
   exports: [
+    NgbModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    GreenhouseCardComponent,
+    EditProfileDialogComponent
   ]
 })
 export class SharedModule { }
