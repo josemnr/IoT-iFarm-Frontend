4199fc64f1aa9a0975a5c3d2e7e978a9fccac0af
josemnr <maninari97@gmail.com>
2021-11-09 22:51:28 -0600

    Update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,28 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { ChartsModule } from 'ng2-charts';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { MaterialModule } from './material/material.module';
 
+import { LayoutComponent } from './components/layout/layout.component';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
 import { GreenhouseCardComponent } from './components/greenhouse-card/greenhouse-card.component';
 import { EditProfileDialogComponent } from './components/edit-profile-dialog/edit-profile-dialog.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
+    HeaderComponent,
+    LayoutComponent,
+    LineChartComponent,
     GreenhouseCardComponent,
-    EditProfileDialogComponent
+    EditProfileDialogComponent,
   ],
   imports: [
     NgbModule,
+    RouterModule,
+    ChartsModule,
     CommonModule,
     MaterialModule,
     ReactiveFormsModule
   ],
   exports: [
     NgbModule,
+    ChartsModule,
     MaterialModule,
+    HeaderComponent,
+    LayoutComponent,
+    LineChartComponent,
     ReactiveFormsModule,
     GreenhouseCardComponent,
     EditProfileDialogComponent
