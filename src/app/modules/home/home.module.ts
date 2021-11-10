82c7e2f9fd38e0b530c50e78ad3fdee4dda03081
josemnr <maninari97@gmail.com>
2021-11-09 22:55:05 -0600

    Update home module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from './../../shared/shared.module';
 
 import { HomeComponent } from './components/home/home.component';
 import { HomeHeaderComponent } from './components/home-header/home-header.component';
 import { HomeCarouselComponent } from './components/home-carousel/home-carousel.component';
 import { CarouselContentComponent } from './components/carousel-content/carousel-content.component';
 
-import { SharedModule } from './../../shared/shared.module';
 
 @NgModule({
   declarations: [
